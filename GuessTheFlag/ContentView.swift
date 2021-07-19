981c579ccb6652fd61ff997e63c7ed7b0598128d
Fray <dpineda@tribalworldwide.gt>
2021-07-18 20:42:42 -0400

    opaque when selection is correct

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -18,29 +18,62 @@ struct FlagImage: View {
             .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 2)
     }
 }
-struct ContentView: View {
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
-    
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0 ... 2)
-    @State private var userScore: Int?
+
+struct BoxButton: View {
+    var number: Int
+    @Binding var showingScore: Bool
+    @Binding var scoreTitle: String
+    @Binding var selectedButton: Int
+    @Binding var userScore: Int?
+    @Binding var shouldMakeOpaque: Bool
+
+    var countries: [String]
+    var correctAnswer: Int
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             userScore = 100
             scoreTitle = "Correct"
+            shouldMakeOpaque = true
         } else {
             userScore = 0
             scoreTitle = "Wrong that is the flag of \(countries[number])"
         }
+        
         showingScore = true
     }
+    
+    var body: some View {
+        Button(action: {
+            self.selectedButton = number
+            flagTapped(number)
+        }) {
+            FlagImage(country: self.countries[number])
+        }
+        .rotationEffect(
+            .degrees(( selectedButton == correctAnswer && selectedButton == number) ? Double(360) : 0)
+        )
+        .opacity((shouldMakeOpaque && selectedButton != number) ? 0.25 : 1.0)
+        .animation(shouldMakeOpaque ? .interpolatingSpring(stiffness: 10.0, damping: 11.0) : nil)
+    }
+}
+struct ContentView: View {
+    @State var showingScore = false
+    @State var scoreTitle = ""
+    @State var selected = 4
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0 ... 2)
+    @State var userScore: Int?
+    @State var shouldmakeOpaque = false
+    
+    private var flagsArray = [FlagImage]()
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0 ... 2)
         userScore = nil
+        shouldmakeOpaque = false
     }
     
     private func getText() -> String {
@@ -69,12 +102,8 @@ struct ContentView: View {
                 }
                 
                 ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        flagTapped(number)
-                    }) {
-                        FlagImage(country: self.countries[number])
-                        
-                    }
+                    BoxButton(number: number,
+                              showingScore: self.$showingScore, scoreTitle: self.$scoreTitle, selectedButton: self.$selected, userScore: self.$userScore, shouldMakeOpaque: self.$shouldmakeOpaque, countries: countries, correctAnswer: correctAnswer)
                     
                     Spacer()
                 }
